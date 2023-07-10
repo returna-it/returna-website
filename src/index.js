0f3fd1503591bd98a3f0762de50cffde1db90584
vincenzopecorella <vincenzo.pecorella.vp@gmail.co>
2023-07-10 23:56:25 +0200

    intermediate commit solved

--- src/index.js
+++ src/index.js
@@ -24,10 +24,10 @@ import { ThemeProvider } from "@aws-amplify/ui-react";
 import { Amplify } from 'aws-amplify';
 
 
-import awsconfig from '/src/aws-exports';
+import awsconfig from 'src/aws-exports';
 
 import "@aws-amplify/ui-react/styles.css";
-import { studioTheme } from "/src/ui-components";
+import { studioTheme } from "src/ui-components";
 
 // styles for this kit
 import "assets/css/bootstrap.min.css";
